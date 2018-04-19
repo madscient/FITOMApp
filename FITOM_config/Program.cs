fe8a94897637f0aebfa9a1089c980dda52a56125
madscient <mad.scientify@gmail.com>
2018-04-19 20:08:34 +0900

    adding: new configurer

--- FITOM_config/Program.cs
+++ FITOM_config/Program.cs
@@ -16,7 +16,7 @@ namespace FITOM_config
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
