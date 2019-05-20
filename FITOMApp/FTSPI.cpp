074dc504130c19858cd32d45056901e105c58bd2
madscient <mad.scientify@gmail.com>
2019-05-20 20:05:20 +0900

    Implementing: FT232H & FT2232 interface

--- FITOMApp/FTSPI.cpp
+++ FITOMApp/FTSPI.cpp
@@ -1,71 +1,11 @@
 #include "stdafx.h"
 #include "FTSPI.h"
 
-CFTInterface::CFTInterface()
-{
-	bValid = Init();
-}
+vector<CFTInterface*> ftInterfaces;
 
-CFTInterface::~CFTInterface()
-{
-	for (int i = 0; i < FTChannel.size(); i++) {
-		FT_STATUS status = FT_Close(FTChannel[i].ftHandle);
-	}
-}
-
-void CFTInterface::BufferPush(UINT32 index, BYTE data)
-{
-	FTChannel[index].cmdbuf[FTChannel[index].wptr++] = data;
-	if (FTChannel[index].wptr == (BUFSIZE - 1)) { BufferFlush(index); }
-}
-
-void CFTInterface::BufferPush(UINT32 index, BYTE* data, UINT32 length)
-{
-	if ((FTChannel[index].wptr + length) >= (BUFSIZE - 1)) { BufferFlush(index); }
-	memcpy(&FTChannel[index].cmdbuf[FTChannel[index].wptr], data, length);
-	FTChannel[index].wptr += length;
-}
-
-FT_STATUS CFTInterface::BufferedRead(UINT32 index, UINT8* buffer, UINT32 sizeToTransfer, UINT32 cs)
-{
-	FT_STATUS ret = FT_OK;
-	return ret;
-}
-
-UINT32 CFTInterface::GetChannelIndex(UINT32 index)
-{
-	if (index < FTChannel.size()) {
-		return FTChannel[index].index;
-	}
-	return UINT32(-1);
-}
-
-FT_HANDLE CFTInterface::GetChannelHandle(UINT32 index)
-{
-	if (index < FTChannel.size() != NULL) {
-		return FTChannel[index].ftHandle;
-	}
-	return FT_HANDLE(0);
-}
-
-void CFTInterface::GetInterfaceDesc(TCHAR* str, int len)
-{
-	sprintf_s(str, len, _T("%s"), description);
-}
-
-void CFTInterface::BufferFlush()
-{
-	for (UINT32 i = 0; i < FTChannel.size(); i++) {
-		FT_STATUS status = BufferFlush(i);
-		assert(status == FT_OK);
-	}
-}
-
-
-BOOL CFTSPI::Init()
+int InitFTInterface()
 {
 	FT_STATUS status = FT_OK;
-
 	DWORD numDevices = 0;
 	status = FT_ListDevices(&numDevices, 0, FT_LIST_NUMBER_ONLY);
 	assert(status == FT_OK);
@@ -77,123 +17,173 @@ BOOL CFTSPI::Init()
 	assert(status == FT_OK);
 
 	for (DWORD i = 0; i < numDevices; i++) {
-		if (devList[i].Type == FT_DEVICE_232H) {
-			sprintf_s(description, _countof(description), _T("%s(%s)"), devList[i].Description, devList[i].SerialNumber);
-			FTHINFO spiinfo;
-			spiinfo.index = i;
-			spiinfo.rptr = spiinfo.wptr = 0;
-			status = FT_Open(i, &spiinfo.ftHandle);
-			assert(status == FT_OK);
-			FTChannel.push_back(spiinfo);
-			status = InitChannel(FTChannel.size() - 1);
-			assert(status == FT_OK);
+		FTHINFO fthinfo;
+		sprintf_s(fthinfo.description, _countof(fthinfo.description), _T("%s(%s)"), devList[i].Description, devList[i].SerialNumber);
+		fthinfo.index = i;
+		fthinfo.rptr = fthinfo.wptr = 0;
+		switch (devList[i].Type)
+		{
+		case FT_DEVICE_232H:
+			break;
+		case FT_DEVICE_2232C:
+			break;
 		}
 	}
 	delete[] devList;
 	return (numDevices != 0);
 }
 
-FT_STATUS CFTSPI::InitChannel(UINT32 index)
+FTHINFO& FTHINFO::operator=(FTHINFO& fth)
+{
+	ftHandle = fth.ftHandle;
+	index = fth.index;
+	rptr = fth.rptr;
+	wptr = fth.wptr;
+	memcpy_s(description, _countof(description), fth.description, _countof(fth.description));
+	memcpy_s(cmdbuf, _countof(cmdbuf), fth.cmdbuf, _countof(fth.cmdbuf));
+	return *this;
+}
+
+CFTInterface::CFTInterface()
+{
+	bValid = Init();
+}
+
+CFTInterface::~CFTInterface()
+{
+	FT_Close(FTChannel.ftHandle);
+}
+
+void CFTInterface::BufferPush(BYTE data)
+{
+	FTChannel.cmdbuf[FTChannel.wptr++] = data;
+	if (FTChannel.wptr == (BUFSIZE - 1)) { BufferFlush(); }
+}
+
+void CFTInterface::BufferPush(BYTE* data, UINT32 length)
+{
+	if ((FTChannel.wptr + length) >= (BUFSIZE - 1)) { BufferFlush(); }
+	memcpy(&FTChannel.cmdbuf[FTChannel.wptr], data, length);
+	FTChannel.wptr += length;
+}
+
+FT_STATUS CFTInterface::BufferedRead(UINT8* buffer, UINT32 sizeToTransfer, UINT32 cs)
+{
+	FT_STATUS ret = FT_OK;
+	return ret;
+}
+
+void CFTInterface::GetInterfaceDesc(TCHAR* str, int len)
+{
+	sprintf_s(str, len, _T("%s"), FTChannel.description);
+}
+
+FT_STATUS CFTInterface::BufferFlush()
 {
 	FT_STATUS ret = FT_OTHER_ERROR;
-	if (index < FTChannel.size()) {
-		//ret = FT_SetBaudRate(SPIChannel[index].ftHandle, 2000000L);	//1Mbps
-		//if (ret != FT_OK) return ret;
-		ret = FT_Purge(FTChannel[index].ftHandle, FT_PURGE_RX | FT_PURGE_TX);
-		if (ret != FT_OK) return ret;
-		::Sleep(10);
-		ret = FT_SetLatencyTimer(FTChannel[index].ftHandle, 1);	//1ms
-		if (ret != FT_OK) return ret;
-		ret = FT_SetBitMode(FTChannel[index].ftHandle, 0x0, 0x00);	//reset mode
-		if (ret != FT_OK) return ret;
-		::Sleep(10);
-		ret = FT_SetBitMode(FTChannel[index].ftHandle, 0x00, 0x02);	//MPSSE mode
-		if (ret != FT_OK) return ret;
-		ret = FT_Purge(FTChannel[index].ftHandle, FT_PURGE_RX);
-		if (ret != FT_OK) return ret;
-		::Sleep(20);
-		BufferPush(index, 0x80);	//Set initial value of ADBUS
-		BufferPush(index, 0xf8);	//
-		BufferPush(index, 0xfb);	//
-		BufferPush(index, 0x82);	//Set initial value of ACBUS
-		BufferPush(index, 0xff);	//
-		BufferPush(index, 0xff);	//
-		BufferPush(index, 0x8a);	//disable initial devide
-		BufferPush(index, 0x86);	//CK devisor
-		BufferPush(index, 0x02);
-		BufferPush(index, 0x00);
-		BufferPush(index, 0x85);	//disable loopback
-		BufferPush(index, 0x8d);	//disable 3 phase clocking
-		ret = BufferFlush(index);
-		::Sleep(100);
-		if (ret != FT_OK) return ret;
+	DWORD written = 0;
+	if (FTChannel.wptr) {
+		//SPIChannel[index].cmdbuf[SPIChannel[index].wptr++] = 0x87;	//force flush
+		ret = FT_Write(FTChannel.ftHandle, FTChannel.cmdbuf, FTChannel.wptr, &written);
+		FTChannel.wptr = 0;
+		//BYTE forceflush = 0x87;
+		//ret = FT_Write(SPIChannel[index].ftHandle, &forceflush, 1, &written);
+	}
+	else {
+		ret = FT_OK;
 	}
 	return ret;
 }
 
 
-FT_STATUS CFTSPI::BufferedWrite(UINT32 index, UINT8* buffer, UINT32 sizeToTransfer, UINT32 cs)
+CFTSPI::CFTSPI(FTHINFO& fth) : CFTInterface()
+{
+	FT_STATUS status = FT_OK;
+	FTChannel = fth;
+	status = FT_Open(FTChannel.index, &FTChannel.ftHandle);
+	assert(status == FT_OK);
+	status = Init();
+	assert(status == FT_OK);
+}
+
+FT_STATUS CFTSPI::Init()
+{
+	FT_STATUS ret = FT_OTHER_ERROR;
+	//ret = FT_SetBaudRate(SPIChannel[index].ftHandle, 2000000L);	//1Mbps
+	//if (ret != FT_OK) return ret;
+	ret = FT_Purge(FTChannel.ftHandle, FT_PURGE_RX | FT_PURGE_TX);
+	if (ret != FT_OK) return ret;
+	::Sleep(10);
+	ret = FT_SetLatencyTimer(FTChannel.ftHandle, 1);	//1ms
+	if (ret != FT_OK) return ret;
+	ret = FT_SetBitMode(FTChannel.ftHandle, 0x0, 0x00);	//reset mode
+	if (ret != FT_OK) return ret;
+	::Sleep(10);
+	ret = FT_SetBitMode(FTChannel.ftHandle, 0x00, 0x02);	//MPSSE mode
+	if (ret != FT_OK) return ret;
+	ret = FT_Purge(FTChannel.ftHandle, FT_PURGE_RX);
+	if (ret != FT_OK) return ret;
+	::Sleep(20);
+	BufferPush(0x80);	//Set initial value of ADBUS
+	BufferPush(0xf8);	//
+	BufferPush(0xfb);	//
+	BufferPush(0x82);	//Set initial value of ACBUS
+	BufferPush(0xff);	//
+	BufferPush(0xff);	//
+	BufferPush(0x8a);	//disable initial devide
+	BufferPush(0x86);	//CK devisor
+	BufferPush(0x02);
+	BufferPush(0x00);
+	BufferPush(0x85);	//disable loopback
+	BufferPush(0x8d);	//disable 3 phase clocking
+	ret = BufferFlush();
+	::Sleep(100);
+	if (ret != FT_OK) return ret;
+	return ret;
+}
+
+
+FT_STATUS CFTSPI::BufferedWrite(UINT8* buffer, UINT32 sizeToTransfer, UINT32 cs)
 {
 	FT_STATUS ret = FT_OK;
 	UINT8 csmask = ~(1 << (cs + 3));
-	if (index < FTChannel.size() && sizeToTransfer) {
+	if (sizeToTransfer) {
 		UINT16 length = sizeToTransfer-1;
-		BufferPush(index, 0x80);	//assert CS
-		BufferPush(index, 0xf8 & csmask);	//
-		BufferPush(index, 0xfb);	//
-		BufferPush(index, 0x11);	//ve+ edge MSB first no read
-		BufferPush(index, (BYTE*)(&length), 2);	//length
-		BufferPush(index, buffer, sizeToTransfer);
-		BufferPush(index, 0x80);	//dessert CS
-		BufferPush(index, 0xf8);	//
-		BufferPush(index, 0xfb);	//
-		BufferFlush(index);
+		BufferPush(0x80);	//assert CS
+		BufferPush(0xf8 & csmask);	//
+		BufferPush(0xfb);	//
+		BufferPush(0x11);	//ve+ edge MSB first no read
+		BufferPush((BYTE*)(&length), 2);	//length
+		BufferPush(buffer, sizeToTransfer);
+		BufferPush(0x80);	//dessert CS
+		BufferPush(0xf8);	//
+		BufferPush(0xfb);	//
+		BufferFlush();
 	}
 	return ret;
 }
 
-FT_STATUS CFTSPI::FT_WriteGPIO(UINT32 index, UINT8 dir, UINT8 value)
+FT_STATUS CFTSPI::FT_WriteGPIO( UINT8 dir, UINT8 value)
 {
 	FT_STATUS ret = FT_OK;
-	if (index < FTChannel.size()) {
-		BufferPush(index, 0x82);	//Set high byte
-		BufferPush(index, value);	//
-		BufferPush(index, dir);	//
-		BufferFlush(index);
-	}
+	BufferPush(0x82);	//Set high byte
+	BufferPush(value);	//
+	BufferPush(dir);	//
+	BufferFlush();
 	return ret;
 }
 
-FT_STATUS CFTSPI::BufferFlush(UINT32 index)
-{
-	FT_STATUS ret = FT_OTHER_ERROR;
-	if (index < FTChannel.size()) {
-		DWORD written = 0;
-		if (FTChannel[index].wptr) {
-			//SPIChannel[index].cmdbuf[SPIChannel[index].wptr++] = 0x87;	//force flush
-			ret = FT_Write(FTChannel[index].ftHandle, FTChannel[index].cmdbuf, FTChannel[index].wptr, &written);
-			FTChannel[index].wptr = 0;
-			//BYTE forceflush = 0x87;
-			//ret = FT_Write(SPIChannel[index].ftHandle, &forceflush, 1, &written);
-		}
-		else {
-			ret = FT_OK;
-		}
-	}
-	return ret;
-}
 
 void CFTSPI::InitialClear()
 {
-	for (int index = 0; index < FTChannel.size(); index++) {
-		BufferPush(index, 0x82);	//Set high byte
-		BufferPush(index, 0x00);	//assert IC
-		BufferPush(index, 0xff);	//dir
-		BufferFlush(index);
-		::Sleep(10);
-		BufferPush(index, 0x82);	//Set high byte
-		BufferPush(index, 0xff);	//dessert IC
-		BufferPush(index, 0xff);	//dir
-		BufferFlush(index);
-	}
+	BufferPush(0x82);	//Set high byte
+	BufferPush(0x00);	//assert IC
+	BufferPush(0xff);	//dir
+	BufferFlush();
+	::Sleep(10);
+	BufferPush(0x82);	//Set high byte
+	BufferPush(0xff);	//dessert IC
+	BufferPush(0xff);	//dir
+	BufferFlush();
 }
+
--- FITOMApp/FTSPI.h
+++ FITOMApp/FTSPI.h
@@ -3,11 +3,17 @@
 #include "../FTDI/ftd2xx.h"
 
 #define BUFSIZE 65536
+
+int InitFTInterface();
+
 class FTHINFO {
 public:
 	FTHINFO() : index(0), ftHandle(0), wptr(0), rptr(0) {};
+	FTHINFO& operator=(FTHINFO& fth);
 	UINT32 index;
 	FT_HANDLE ftHandle;
+	TCHAR description[96];
+	TCHAR serialnumber[96];
 	BYTE cmdbuf[BUFSIZE];
 	int wptr;
 	int rptr;
@@ -16,42 +22,38 @@ public:
 class CFTInterface {
 protected:
 	BOOL bValid;
-	TCHAR description[96];
-	std::vector<FTHINFO> FTChannel;
-	virtual void BufferPush(UINT32 index, BYTE data);
-	virtual void BufferPush(UINT32 index, BYTE* buf, UINT32 length);
+	FTHINFO FTChannel;
+	virtual void BufferPush(BYTE data);
+	virtual void BufferPush(BYTE* buf, UINT32 length);
 public:
 	CFTInterface();
 	~CFTInterface();
 	virtual BOOL IsValid() { return bValid; };
-	virtual UINT32 GetChannels() { return FTChannel.size(); };
-	virtual UINT32 GetChannelIndex(UINT32 index);
 	virtual void GetInterfaceDesc(TCHAR* str, int len);
-	virtual FT_HANDLE GetChannelHandle(UINT32 index);
-	virtual void BufferFlush();
+	virtual FT_HANDLE GetHandle() { return FTChannel.ftHandle; };
+	virtual FT_STATUS BufferFlush();
 	//to be implemented on derived class
-	virtual BOOL Init() { return false; };
+	virtual FT_STATUS Init() { return FT_NOT_SUPPORTED; };
 	virtual void InitialClear() = 0;
-	//virtual FT_STATUS OpenChannel(UINT32 index, FT_HANDLE* handle) = 0;
-	virtual FT_STATUS InitChannel(UINT32 index) = 0;
-	//virtual FT_STATUS CloseChannel(UINT32 index) = 0;
-	virtual FT_STATUS BufferedRead(UINT32 index, UINT8* buffer, UINT32 sizeToTransfer, UINT32 cs);
-	virtual FT_STATUS BufferedWrite(UINT32 index, UINT8* buffer, UINT32 sizeToTransfer, UINT32 cs) = 0;
-	virtual FT_STATUS FT_WriteGPIO(UINT32 index, UINT8 dir, UINT8 value) = 0;
-	virtual FT_STATUS BufferFlush(UINT32 index) = 0;
+	virtual FT_STATUS BufferedRead(UINT8* buffer, UINT32 sizeToTransfer, UINT32 cs);
+	virtual FT_STATUS BufferedWrite(UINT8* buffer, UINT32 sizeToTransfer, UINT32 cs) = 0;
+	virtual FT_STATUS FT_WriteGPIO(UINT8 dir, UINT8 value) = 0;
 };
 
 class CFTSPI : public CFTInterface {
 public:
-	CFTSPI() : CFTInterface() {};
-	virtual BOOL Init();
+	CFTSPI(FTHINFO& fth);
+	virtual FT_STATUS Init();
 	virtual void InitialClear();
-	//virtual FT_STATUS OpenChannel(UINT32 index, FT_HANDLE* handle);
-	virtual FT_STATUS InitChannel(UINT32 index);
-	//virtual FT_STATUS CloseChannel(UINT32 index);
-	virtual FT_STATUS BufferedWrite(UINT32 index, UINT8* buffer, UINT32 sizeToTransfer, UINT32 cs);
-	virtual FT_STATUS FT_WriteGPIO(UINT32 index, UINT8 dir, UINT8 value);
-	virtual FT_STATUS BufferFlush(UINT32 index);
+	virtual FT_STATUS BufferedWrite(UINT8* buffer, UINT32 sizeToTransfer, UINT32 cs);
+	virtual FT_STATUS FT_WriteGPIO(UINT8 dir, UINT8 value);
 };
 
-
+class CFTHBE : public CFTInterface {
+public:
+	CFTHBE(FTHINFO& fth);
+	virtual FT_STATUS Init();
+	virtual void InitialClear();
+	virtual FT_STATUS BufferedWrite(UINT8* buffer, UINT32 sizeToTransfer, UINT32 cs);
+	virtual FT_STATUS FT_WriteGPIO(UINT8 dir, UINT8 value);
+};
